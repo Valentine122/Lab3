2d1313b44890f96f78bdbb5442be3e6a18fde90a
Valentine122 <56410350+Valentine122@users.noreply.github.com>
2020-03-22 00:51:49 +0200

    Update SetsForTheBar.java

--- src/main/java/ua/lviv/iot/stuffForBar/model/SetsForTheBar.java
+++ src/main/java/ua/lviv/iot/stuffForBar/model/SetsForTheBar.java
@@ -1,28 +1,9 @@
 package ua.lviv.iot.stuffForBar.model;
 
 public class SetsForTheBar extends AbstractBar {
-    private int numberOfItemsInPerSet;
-    private String amountOfStuffs;
-
-    public int getNumberOfItemsInPerSet() {
-        return numberOfItemsInPerSet;
+    public SetsForTheBar(int priceInUAH, ShapeOfIce shapeOfIce, int productionYear) {
+        super(priceInUAH, shapeOfIce, productionYear);
     }
 
-    public void setNumberOfItemsInPerSet(int numberOfItemsInPerSet) {
-        this.numberOfItemsInPerSet = numberOfItemsInPerSet;
-    }
-
-    public String getAmountOfStuffs() {
-        return amountOfStuffs;
-    }
-
-    public void setAmountOfStuffs(String amountOfStuffs) {
-        this.amountOfStuffs = amountOfStuffs;
-    }
-
-    public SetsForTheBar(String material, float priceInUAH, int productionYear, String producerName, int weightInGrams, int numberOfItemsInPerSet, String amountOfStuffs) {
-        super(material, priceInUAH, productionYear, producerName, weightInGrams);
-        this.numberOfItemsInPerSet = numberOfItemsInPerSet;
-        this.amountOfStuffs = amountOfStuffs;
-    }
 }
+
