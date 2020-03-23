dd854771b9e6d66555a2236f4d1d941b623fc6f8
Valentine122 <56410350+Valentine122@users.noreply.github.com>
2020-03-23 19:27:34 +0200

    Update FormsForIce.java

--- src/main/java/ua/lviv/iot/stuffForBar/model/FormsForIce.java
+++ src/main/java/ua/lviv/iot/stuffForBar/model/FormsForIce.java
@@ -1,9 +1,21 @@
 package ua.lviv.iot.stuffForBar.model;
 
 public class FormsForIce extends AbstractBar {
+   private int volume;
+
     public FormsForIce(int priceInUAH, ShapeOfIce shapeOfIce, int productionYear) {
         super(priceInUAH, shapeOfIce, productionYear);
+
     }
 
-}
+   public String getCubes(){
+       return super.getCubes() + ", " + "volume";
+   }
 
+   public int getVolume(){
+       return volume;
+   }
+   public String toWhiskey(){
+       return super.toWhiskey() + ", " + getVolume();
+   }
+}
