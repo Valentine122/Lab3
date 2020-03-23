c5749857e4401071763ce7937c298395c3b1d8c9
Valentine122 <56410350+Valentine122@users.noreply.github.com>
2020-03-23 19:28:54 +0200

    Update SetsForTheBar.java

--- src/main/java/ua/lviv/iot/stuffForBar/model/SetsForTheBar.java
+++ src/main/java/ua/lviv/iot/stuffForBar/model/SetsForTheBar.java
@@ -1,9 +1,25 @@
 package ua.lviv.iot.stuffForBar.model;
 
 public class SetsForTheBar extends AbstractBar {
+    private int numOfSets;
+
     public SetsForTheBar(int priceInUAH, ShapeOfIce shapeOfIce, int productionYear) {
         super(priceInUAH, shapeOfIce, productionYear);
+
+    }
+
+    public int getNumOfSets(){
+        return numOfSets;
+    }
+
+    public String getCubes(){
+        return super.getCubes() + ", " + "numOfSets";
+    }
+
+    public String toWhiskey(){
+        return super.toWhiskey() + ", " + getNumOfSets();
     }
 
 }
+
 
