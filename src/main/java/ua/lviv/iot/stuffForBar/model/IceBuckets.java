737bd022e13602cf9e8b3b00f4f83a773dec0efb
Valentine122 <56410350+Valentine122@users.noreply.github.com>
2020-03-23 19:28:01 +0200

    Update IceBuckets.java

--- src/main/java/ua/lviv/iot/stuffForBar/model/IceBuckets.java
+++ src/main/java/ua/lviv/iot/stuffForBar/model/IceBuckets.java
@@ -1,9 +1,25 @@
 package ua.lviv.iot.stuffForBar.model;
 
 public class IceBuckets extends AbstractBar {
+    private int numOfBottles;
+
     public IceBuckets(int priceInUAH, ShapeOfIce shapeOfIce, int productionYear) {
         super(priceInUAH, shapeOfIce, productionYear);
+
+    }
+
+    public int getNumOfBottles(){
+        return numOfBottles;
+    }
+
+    public String getCubes(){
+        return super.getCubes() + ", " + "numOfBottles";
+    }
+
+    public String toWhiskey() {
+        return super.toWhiskey() + ", " + getNumOfBottles();
     }
 
 }
+
 
