9b8cbc557d89ff546bb867548a8dd3fa1172976f
Valentine122 <56410350+Valentine122@users.noreply.github.com>
2020-03-23 19:59:21 +0200

    Update FormsForIce.java

--- src/main/java/ua/lviv/iot/stuffForBar/model/FormsForIce.java
+++ src/main/java/ua/lviv/iot/stuffForBar/model/FormsForIce.java
@@ -1,21 +1,30 @@
-package ua.lviv.iot.stuffForBar.model;
+import ua.lviv.iot.stuffForBar.model.ShapeOfIce;
 
 public class FormsForIce extends AbstractBar {
-   private int volume;
+private int amountOfIce;
+private ShapeOfIce shapeOfIce;
 
-    public FormsForIce(int priceInUAH, ShapeOfIce shapeOfIce, int productionYear) {
-        super(priceInUAH, shapeOfIce, productionYear);
-
-    }
-
-   public String getCubes(){
-       return super.getCubes() + ", " + "volume";
-   }
-
-   public int getVolume(){
-       return volume;
-   }
-   public String toWhiskey(){
-       return super.toWhiskey() + ", " + getVolume();
-   }
+public int getAmountOfIce() {
+return amountOfIce;
 }
+
+public void setAmountOfIce(int amountOfIce) {
+this.amountOfIce = amountOfIce;
+}
+
+public ShapeOfIce getShapeOfIce() {
+return shapeOfIce;
+}
+
+public void setShapeOfIce(ShapeOfIce shapeOfIce) {
+this.shapeOfIce = shapeOfIce;
+}
+
+public FormsForIce(String material, float priceInUAH, int productionYear, String producerName, int weightInGrams, int amountOfIce, ShapeOfIce shapeOfIce) {
+super(material, priceInUAH, productionYear, producerName, weightInGrams);
+this.amountOfIce = amountOfIce;
+this.shapeOfIce = shapeOfIce;
+}
+
+}
+
